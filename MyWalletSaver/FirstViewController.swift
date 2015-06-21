ad8635992d05194860e392de21775bb36395e84a
Dmitry Sokolov <hidan446@gmail.com>
2015-06-21 16:55:12 -0700

    Initial Commit

---
+++ MyWalletSaver/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyWalletSaver
+//
+//  Created by Dmitry Sokolov on 6/21/15.
+//  Copyright (c) 2015 Dmitry Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
